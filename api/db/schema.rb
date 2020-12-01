044b52df7c9dd5ae3df6ee2da514646926e8c0e0
annyzo <rannyzo94@gmail.com>
2020-12-01 17:07:16 +0300

    gestion saison

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_12_01_142533) do
-=======
-ActiveRecord::Schema.define(version: 2020_11_30_142423) do
->>>>>>> c189e059d3bd7a03ff2396831b99b05a2a57ee7b
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
