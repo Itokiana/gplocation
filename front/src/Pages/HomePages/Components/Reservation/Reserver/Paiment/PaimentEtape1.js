4f6b18105071629d64ca91c7da51a71e6d8d2786
Sambatra2020 <natarabem@gmail.com>
2020-12-08 16:07:07 +0300

    dynamisme tarif et account

--- front/src/Pages/HomePages/Components/Reservation/Reserver/Paiment/PaimentEtape1.js
+++ front/src/Pages/HomePages/Components/Reservation/Reserver/Paiment/PaimentEtape1.js
@@ -82,7 +82,7 @@ function PaimentEtape1(props) {
                                         </label>
                                     </fieldset>
                                 <h3 class="form-connec-h3">Total à payer : acompte de 100 €</h3>
-                                <p class="paddingp">Puis 188 € à régler à la remise des clés par carte bancaire, espèce ou chèque </p>
+                                <p class="paddingp">Puis {parseInt(props.prix)-100} € à régler à la remise des clés par carte bancaire, espèce ou chèque </p>
                                 <h3 class="paiementcarte cc_cursor">Paiement sécurisé par carte bancaire :</h3>
                                 
                                     <Formik
