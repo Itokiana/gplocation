efd81e562917cb7bca38ae843e2806c5f0747290
Annyzo <rannyzo94@gmail.com>
2020-12-12 20:14:20 +0300

    haka j

--- admin/src/pages/ConfigurationAgence/GestionOF/Components/GestionJF/GestionJF.js
+++ admin/src/pages/ConfigurationAgence/GestionOF/Components/GestionJF/GestionJF.js
@@ -23,7 +23,7 @@ class GestionJF extends Component {
             nombreLigneUn: [...this.state.nombreLigneUn,this.state.nombreLigneUn.pop() + 1],
             //idUn: [...this.state.idUn, this.state.idGlobal.pop() +1]
         })
-        
+        console.log("tab1", this.state.nombreLigneUn)
     }
     ajoutNewDeuxJour =() =>{
         this.setState({
@@ -92,10 +92,7 @@ class GestionJF extends Component {
                 initVal: inValue  
             }) 
             console.log("objet tout", this.state.objetData)
-            console.log("id tout", this.state.idGlobal) 
-            console.log("Tab 1", this.state.nombreLigneDeux)
-            
-
+            console.log("id tout", this.state.idGlobal)    
         })
        
     
